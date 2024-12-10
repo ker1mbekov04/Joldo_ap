d86cb6ae23ba498e1151f35ccc92fb2c5f2c50be
mirlanov570 <mirlanuulunursultan90@gmail.com>
2024-12-10 16:00:34 +0600

    zzz

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,6 +22,12 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+
+    // Включение ViewBinding
+    viewBinding {
+       var enabled = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -30,12 +36,18 @@ android {
 
 dependencies {
 
+    // Core dependencies
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // Firebase dependencies
     implementation("com.google.firebase:firebase-auth:23.0.0")
+    implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
+    implementation("com.google.firebase:firebase-analytics")
+
+    // Testing dependencies
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
 }
